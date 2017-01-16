027aa17288cfbf97dd0991cfd7bcd8f1bbc179ad
dbaston <dbaston@maponics.com>
2017-01-15 20:39:59 -0500

    Some progress on LWLINE adapter

--- main.cpp
+++ main.cpp
@@ -17,19 +17,39 @@ namespace boost {
     namespace geometry {
         namespace traits {
             // Adapt LWLINE to Boost.Geometry
-            //template<>
-            //struct tag<LWLINE>
-            //{
-            //    typedef linestring_tag type;
-            //};
-
-            //template<>
-            //struct range_iterator<LWLINE>
-            //{
-            //    typedef std::vector<LWPOINT*>::iterator type;
-            //};
+            template<>
+            struct tag<LWLINE> {
+                using type= linestring_tag;
+            };
         }
     }
+}
+
+namespace boost {
+    //template<>
+    //struct range_iterator<LWLINE> {
+    //    using type= ptarray_iterator<POINT2D*>;
+    //};
+
+    template<>
+    struct range_const_iterator<LWLINE> {
+        using type= ptarray_const_iterator<POINT2D>;
+    };
+
+    template<>
+    struct range_value<LWLINE> {
+        using type= POINT2D;
+    };
+}
+
+template<typename T>
+inline ptarray_const_iterator<T> range_begin(LWLINE& lwline) {
+    return cbegin<T>(*lwline.points);
+}
+
+template<typename T>
+inline ptarray_const_iterator<T> range_end(LWLINE& lwline) {
+    return cend<T>(*lwline.points);
 }
 
 using namespace std;
@@ -38,21 +58,26 @@ int main() {
     POINT2D p1 { 3, 5 };
     LWPOINT* p2 = lwpoint_make2d(0, 3.0, 7.0);
 
+    LWLINE* line = lwline_construct_empty(4326, 0, 0);
+    lwline_add_lwpoint(line, lwpoint_make2d(0, 4, 9), 0);
+    lwline_add_lwpoint(line, lwpoint_make2d(0, 2, 97), 1);
+
     cout << boost::geometry::distance(p1, *p2) << endl;
+    cout << boost::geometry::distance(line, line) << endl;
 
     POINTARRAY* pta = ptarray_construct_empty(1, 1, 4);
     pta = ptarray_addPoint(pta, (uint8_t*) &p1, 2, 0);
     pta = ptarray_addPoint(pta, (uint8_t*) &p1, 2, 0);
     pta = ptarray_addPoint(pta, (uint8_t*) &p1, 2, 0);
 
-    for (ptarray_iterator<POINT2D*> it = begin<POINT2D*>(*pta); it != end<POINT2D*>(*pta); ++it) {
-        POINT2D* p = *it;
-        std::cout << p->x << " " << p->y << std::endl;
+    for (auto it = cbegin<POINT2D>(*pta); it != cend<POINT2D>(*pta); ++it) {
+        auto p = *it;
+        std::cout << p.x << " " << p.y << std::endl;
     }
 
-    for (ptarray_iterator<POINT4D*> it = begin<POINT4D*>(*pta); it != end<POINT4D*>(*pta); ++it) {
-        POINT4D* p = *it;
-        std::cout << p->x << " " << p->y << " " << p->z << " " << p-> m << std::endl;
+    for (auto it = cbegin<POINT4D>(*pta); it != cend<POINT4D>(*pta); ++it) {
+        auto p = *it;
+        std::cout << p.x << " " << p.y << " " << p.z << " " << p.m << std::endl;
     }
 
     return 0;
--- ptarray_iterator.h
+++ ptarray_iterator.h
@@ -6,101 +6,105 @@ extern "C" {
 }
 
 template<typename T>
-class ptarray_iterator
+class ptarray_const_iterator
 {
 public:
     using iterator_category= std::random_access_iterator_tag;
     using difference_type= int;
     using value_type= T;
+    using pointer= decltype(std::declval<T>());
+    //using const_type= typename std::add_const<value_type>::type;
+    using reference= const value_type&;
 
 private:
-    POINTARRAY* m_pta;
+    const POINTARRAY* m_pta;
     difference_type m_pos;
 
-
 public:
-    ptarray_iterator(POINTARRAY* pta) : m_pta{pta}, m_pos{0} {}
-    ptarray_iterator(POINTARRAY* pta, difference_type pos) : m_pta{pta}, m_pos{pos} {}
+    ptarray_const_iterator() : m_pta{nullptr}, m_pos{0} {}
+    ptarray_const_iterator(const POINTARRAY* pta) : m_pta{pta}, m_pos{0} {}
+    ptarray_const_iterator(const POINTARRAY* pta, difference_type pos) : m_pta{pta}, m_pos{pos} {}
 
-    value_type operator*() const;
+    reference operator*() const;
 
-    ptarray_iterator& operator++() {
+    ptarray_const_iterator& operator++() {
         ++m_pos;
         return *this;
     }
 
-    ptarray_iterator& operator+=(difference_type n) {
+    ptarray_const_iterator operator++(int junk) {
+        return ptarray_const_iterator(m_pta, m_pos++);
+    }
+
+    ptarray_const_iterator& operator+=(difference_type n) {
         m_pos += n;
         return *this;
     }
 
-    ptarray_iterator& operator-=(difference_type n) {
+    ptarray_const_iterator& operator-=(difference_type n) {
         return *this += -n;
     }
 
-    ptarray_iterator operator+(difference_type n) const {
-        return ptarray_iterator(m_pta, m_pos + n);
+    ptarray_const_iterator operator+(difference_type n) const {
+        return ptarray_const_iterator(m_pta, m_pos + n);
     }
 
-    ptarray_iterator operator-(difference_type n) const {
-        return ptarray_iterator(m_pta, m_pos - n);
+    ptarray_const_iterator operator-(difference_type n) const {
+        return ptarray_const_iterator(m_pta, m_pos - n);
     }
 
-    difference_type operator-(ptarray_iterator & other) {
+    difference_type operator-(const ptarray_const_iterator & other) const {
         return m_pos - other.m_pos;
     }
 
-    bool operator==(const ptarray_iterator & other) const {
+    bool operator==(const ptarray_const_iterator & other) const {
         return m_pta == other.m_pta && m_pos == other.m_pos;
     }
 
-    bool operator!=(const ptarray_iterator & other) const {
+    bool operator!=(const ptarray_const_iterator & other) const {
         return !(*this == other);
     }
 
-    bool operator>(const ptarray_iterator & other) const {
+    bool operator>(const ptarray_const_iterator & other) const {
         return m_pos > other.m_pos;
     }
 
-    bool operator>=(const ptarray_iterator & other) const {
+    bool operator>=(const ptarray_const_iterator & other) const {
         return m_pos >= other.m_pos;
     }
 
-    bool operator<(const ptarray_iterator & other) const {
+    bool operator<(const ptarray_const_iterator & other) const {
         return m_pos < other.m_pos;
     }
 
-    bool operator<=(const ptarray_iterator & other) const {
+    bool operator<=(const ptarray_const_iterator & other) const {
         return m_pos <= other.m_pos;
     }
 };
 
 template<>
-POINT2D* ptarray_iterator<POINT2D*>::operator*() const {
-    // TODO don't cast like this
-    return (POINT2D*) getPoint2d_cp(m_pta, m_pos);
+const POINT2D & ptarray_const_iterator<POINT2D>::operator*() const {
+    return *((POINT2D*) getPoint2d_cp(m_pta, m_pos));
 }
 
 template<>
-POINT3DZ* ptarray_iterator<POINT3DZ*>::operator*() const {
-    // TODO don't cast like this
-    return (POINT3DZ*) getPoint3dz_cp(m_pta, m_pos);
+const POINT3DZ & ptarray_const_iterator<POINT3DZ>::operator*() const {
+    return *((POINT3DZ*) getPoint3dz_cp(m_pta, m_pos));
 }
 
 template<>
-POINT4D* ptarray_iterator<POINT4D*>::operator*() const {
-    // TODO don't cast like this
-    return (POINT4D*) getPoint4d_cp(m_pta, m_pos);
+const POINT4D & ptarray_const_iterator<POINT4D>::operator*() const {
+    return *((POINT4D*) getPoint4d_cp(m_pta, m_pos));
 }
 
 template<typename T>
-ptarray_iterator<T> begin(POINTARRAY & pta) {
-    return ptarray_iterator<T>(&pta);
+ptarray_const_iterator<T> cbegin(const POINTARRAY & pta) {
+    return ptarray_const_iterator<T>(&pta);
 }
 
 template<typename T>
-ptarray_iterator<T> end(POINTARRAY & pta) {
-    return ptarray_iterator<T>(&pta, pta.npoints);
+ptarray_const_iterator<T> cend(const POINTARRAY & pta) {
+    return ptarray_const_iterator<T>(&pta, pta.npoints);
 }
 
 
